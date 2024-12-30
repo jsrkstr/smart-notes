e2714c6105c3a2840fe13aa607e85feb700166c6
Sachin Saluja <sachin.saluja132@gmail.com>
2024-12-30 15:20:49 +0000

    Automatic commit before Assistant edits

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout() {
             presentation: 'modal',
             animationDuration: 3000,
           }}>
-          <Stack.Screen name="app" options={{ headerShown: false }} />
+          <Stack.Screen name="app"/>
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" hidden={false} />
--- app/app.tsx
+++ app/app.tsx
@@ -1,11 +1,12 @@
 import { Stack } from "expo-router";
 import HomeScreen from "./screens/HomeScreen";
+import { ThemedText } from "@/components/ThemedText";
 
 export default function AppScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'App!' }} />
-      <HomeScreen />
+      {/* <Stack.Screen options={{ title: 'App!' }} /> */}
+      <ThemedText type="title">This screen doesn't exist.</ThemedText>
     </>
   );
 }
